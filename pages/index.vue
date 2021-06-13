e796fc72226df873640736da0f7f07c8804c5c07
shiogao <github@shiogao.com>
2021-06-13 02:41:22 +0000

    change sample code

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>template</div>
+  <div>
+    counter: {{ counter }}
+    <button @click="plus">
+      plus
+    </button>
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    plus () {
+      this.counter++
+    }
+  }
+})
+</script>
